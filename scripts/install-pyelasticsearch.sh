0e63c85a43147c052bd122504c603adbea838e09
z3ndrag0n <z3ndrag0n@me.com>
2014-09-30 22:04:38 +1000

    The version of plaso shipping with SIFT 3 doesn't have ElasticSearch support, so update it.

--- scripts/install-pyelasticsearch.sh
+++ scripts/install-pyelasticsearch.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Make sure plaso is up-to-date before continuing (thanks BMG)
+sudo apt-get update
+sudo apt-get install python-plaso
 # Download and install python library to interact with Elasticsearch
 git clone https://github.com/rhec/pyelasticsearch.git
 cd pyelasticsearch
