7fcbc32dc408bb7b075fef4ce9f029d3703778c7
z3ndrag0n <z3ndrag0n@me.com>
2014-10-02 00:02:21 +1000

    Two updates - one: update to the latest version of Jenkins, not a particular version; two: once Jenkins has been updated, wait for it to start properly before attempting to install plugins, jobs and views.

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -15,10 +15,13 @@ sudo service jenkins restart
 
 #update Jenkins
 cd /usr/share/jenkins
-sudo mv jenkins.war jenkins-apt-install.war
-sudo curl -o jenkins.war -L http://updates.jenkins-ci.org/download/war/1.581/jenkins.war
+sudo mv jenkins.war jenkins-last-install.war
+sudo curl -o jenkins.war -L http://updates.jenkins-ci.org/download/war/latest/jenkins.war
 cd ~
 sudo service jenkins restart
+
+#wait for Jenkins to start
+until wget http://localhost:8080/ 2>&1 | grep "response" | grep -c "200 OK"; do echo Sleeping 15 seconds waiting for Jenkins to start... ; sleep 15 ; done
 
 #install plugins
 java -jar /run/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin parameterized-trigger
