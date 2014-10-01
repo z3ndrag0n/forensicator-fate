769b0004d5965dd4476d32d00da3122d177ff31b
z3ndrag0n <z3ndrag0n@me.com>
2014-10-01 19:57:45 +1000

    If you sudo a command naively, remember *your* shell owns any I/O redirects - LOL.

--- scripts/create-shares.sh
+++ scripts/create-shares.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo mkdir /ioc /blacklist /whitelist /reverse /artifacts
 sudo mv /etc/samba/smb.conf /etc/samba/smb.conf-default
-sudo cat >/etc/samba/smb.conf-fate <<EOF
+sudo su - root -c "cat >/etc/samba/smb.conf-fate" <<EOF
 
 [ioc]
 	path = /ioc
@@ -33,5 +33,5 @@ sudo cat >/etc/samba/smb.conf-fate <<EOF
 	browseable = yes
 	guest ok = yes
 EOF
-sudo cat /etc/samba/smb.conf-default /etc/samba/smb.conf-fate >/etc/samba/smb.conf	
+sudo su - root -c "cat /etc/samba/smb.conf-default /etc/samba/smb.conf-fate >/etc/samba/smb.conf"
 sudo service smbd restart
