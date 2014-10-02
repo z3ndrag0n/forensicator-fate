4b08d64e69c62f6f0666b939efd656902e05d7f1
z3ndrag0n <z3ndrag0n@me.com>
2014-10-03 04:34:22 +1000

    Setup the database so we can connect to it w/out authenticating. Create the cases table.

--- scripts/install-pg.sh
+++ scripts/install-pg.sh
@@ -7,3 +7,14 @@ create database webpy;
 grant all on database webpy to webpy;
 alter role webpy with login;
 EOF
+sudo ed /etc/postgresql/9.1/main/pg_hba.conf <<EOF
+/^local.*peer$
+s/peer/trust/
+w
+q
+EOF
+sudo service postgresql restart
+psql -U webpy -d webpy <<EOF
+create table cases (id SERIAL, casename varchar, memory_image varchar, disk_image varchar, disk_name varchar, timezone varchar, volatility_profile varchar, notes varchar, keywords varchar);
+\q
+EOF
