ee77b7b60b3f27e817982b0ddf8633d78185f868
z3ndrag0n <z3ndrag0n@me.com>
2014-10-03 17:50:27 +1000

    Run all five scripts so that instead of the user having to run all five, they just run one.

---
+++ scripts/ffate-bundle-installer.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+forensicator-fate/scripts/install-pyelasticsearch.sh
+forensicator-fate/scripts/create-shares.sh
+forensicator-fate/scripts/install-jenkins.sh
+forensicator-fate/scripts/install-pg.sh
+forensicator-fate/scripts/install-ffate.sh
