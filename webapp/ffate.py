9acdc2ad202d787f113b25ca4b0a775c06562904
z3ndrag0n <z3ndrag0n@me.com>
2014-10-05 06:13:01 +1100

    Fixed the Reverse Engineering tab in the Forensicator FATE Case Manager.

--- webapp/ffate.py
+++ webapp/ffate.py
@@ -1,8 +1,8 @@
-import web
+import web, os, hashlib
 
 urls = (
     '/', 'index',
-    '/cases', 'cases',
+    '/reverse', 'reverse',
     '/search', 'search',
     '/add','add'
 )
@@ -12,7 +12,8 @@ db = web.database(dbn='postgres', db='webpy', user='webpy', pw='')
 render = web.template.render('/var/www/templates')
 
 web.template.Template.globals.update(dict(
-  render = render
+  render = render,
+  getsize = os.path.getsize,
 ))
 
 
@@ -31,6 +32,16 @@ class add:
         n = db.insert('cases', casename=i.casename,memory_image=i.memory_image,disk_image=i.disk_image,disk_name=i.disk_name,timezone=i.timezone,volatility_profile=i.volatility_profile,notes=i.notes,case_keywords=i.case_keywords)
         raise web.seeother('/')
 
+class reverse:
+    def GET(self):
+        name_hash = []
+        for (dirname, dirs, files) in os.walk('/reverse'):
+             for filename in files:
+                 thefile = os.path.join(dirname,filename)
+                 name_hash.append(tuple([thefile, hashlib.md5(open(thefile, 'r').read()).hexdigest()]))
+
+        return render.re_listing(name_hash)
+
 class search:
     def POST(self):
         i = web.input()
@@ -48,4 +59,3 @@ if __name__ == "__main__":
 
 app = web.application(urls, globals())
 application = app.wsgifunc()
-
