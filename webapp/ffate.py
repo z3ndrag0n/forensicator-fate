ec54c8e76874d83e61dbfe82d353900272d70599
z3ndrag0n <z3ndrag0n@me.com>
2014-10-07 02:51:15 +1100

    Asynchronously invoke Jenkins, then redirect the browser back to the page we were on

--- webapp/ffate.py
+++ webapp/ffate.py
@@ -1,10 +1,14 @@
 import web, os, hashlib
+import requests
+import threading
+import time
 
 urls = (
     '/', 'index',
     '/reverse', 'reverse',
     '/search', 'search',
-    '/add','add'
+    '/add','add',
+    '/process', 'process'
 )
 
 db = web.database(dbn='postgres', db='webpy', user='webpy', pw='')
@@ -22,15 +26,13 @@ class index:
         jenkins_url = web.ctx.homedomain + ":8080/"
         return render.tabbed("Forensicator FATE", jenkins_url)
 
-class cases:
-    def GET(self):
-        return render.cases()
 
 class add:
     def POST(self):
         i = web.input()
         n = db.insert('cases', casename=i.casename,memory_image=i.memory_image,disk_image=i.disk_image,disk_name=i.disk_name,timezone=i.timezone,volatility_profile=i.volatility_profile,notes=i.notes,case_keywords=i.case_keywords)
         raise web.seeother('/')
+
 
 class reverse:
     def GET(self):
@@ -42,15 +44,27 @@ class reverse:
 
         return render.re_listing(name_hash)
 
+
 class search:
+    # def POST(self):
+        # i = web.input()
+        # print i
+        # return render.listing(db.select('cases',what='*',order='id',limit=10000))
+    def GET(self):
+        return render.listing(db.select('cases',what='*',order='id',limit=10000), 'process')
+
+
+class process:
     def POST(self):
         i = web.input()
-        print i
-        return render.listing(db.select('cases',what='*',order='id',limit=10000))
-    def GET(self):
         jenkins_url = web.ctx.homedomain + ":8080/"
         jenkins_job_url = jenkins_url + "job/findWindowsEvidence/buildWithParameters"
-        return render.listing(db.select('cases',what='*',order='id',limit=10000), jenkins_job_url)
+        jenkins_job_url_with_params = jenkins_job_url + "?CASE_NAME=" + i.CASE_NAME + "&MEMORY_IMAGE_FILE=" + i.MEMORY_IMAGE_FILE + "&DISK_IMAGE_FILE=" + i.DISK_IMAGE_FILE + "&DISK_NAME=" + i.DISK_NAME + "&TIMEZONE=" + i.TIMEZONE + "&VOLATILITY_PROFILE=" + i.VOLATILITY_PROFILE
+        threading.Thread(target=requests.get, args=(jenkins_job_url_with_params,)).start()
+        # print jenkins_job_url_with_params
+        # print i
+        # time.sleep(1)
+        raise web.seeother('/')
 
 
 if __name__ == "__main__":
