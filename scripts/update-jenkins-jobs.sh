2e1607bc24b9e59660b1d3c2d29fc2d6c3618521
Barry W Anderson <z3ndrag0n@me.com>
2014-10-15 12:50:23 +0000

    Change into the jobs directory before attempting to reload the jobs, otherwise the filenames and jobnames don't match.

--- scripts/update-jenkins-jobs.sh
+++ scripts/update-jenkins-jobs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #update jobs
-for job in `ls -1 forensicator-fate/jenkins/jobs`; do echo Updating job $job; java -jar /run/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job $job <${job}.xml; done
+cd forensicator-fate/jenkins/jobs
+for job in `ls -1`; do echo Updating job $job; java -jar /run/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job $job <${job}.xml; done
