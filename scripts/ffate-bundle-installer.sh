611707600d4724d6a394b8bb655403d46857d138
Barry W Anderson <z3ndrag0n@me.com>
2014-10-22 09:05:00 +0000

    Updated the bundle installer, so if you start it as root, it will su to sansforensics. Otherwise the jenkins job installation fails.

--- scripts/ffate-bundle-installer.sh
+++ scripts/ffate-bundle-installer.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
+if [[ $USER = "root" ]]; then
+    echo "You're running this script as root. Bad things will happen."
+    echo "I'll su to sansforensics and run again now."
+    su - sansforensics -c "forensicator-fate/scripts/ffate-bundle-installer.sh"
+elif [[ $USER = "sansforensics" ]]; then
 echo "The SANS Gold Paper says to run me, then set up your ELK VM and manually"
 echo "setup the kibana dash. However, if you are willing to set up your ELK"
 echo "VM first, I'll automagically setup Kibana for you. Go ahead (I'll wait)..."
@@ -14,3 +19,7 @@ forensicator-fate/scripts/install-jenkins.sh
 forensicator-fate/scripts/install-pg.sh ${ELK_IP}
 forensicator-fate/scripts/install-ffate.sh
 cp forensicator-fate/ffate.pdf Desktop/
+else
+echo "You started this script as neither sansforensics (correct), nor root"
+echo "(incorrect, but understandable). I can't guess your intention. Giving up."
+fi
