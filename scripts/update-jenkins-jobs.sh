883f9ff9489ae43f1e33f62024467fb29988a564
Barry W Anderson <z3ndrag0n@me.com>
2014-10-28 02:26:57 +0000

    BMG found I had borked the update script and the "Create New Case" button was only showing where there were cases.

--- scripts/update-jenkins-jobs.sh
+++ scripts/update-jenkins-jobs.sh
@@ -2,8 +2,9 @@
 
 #update jobs
 cd forensicator-fate/jenkins/jobs
-for job in `ls -1`; do echo Updating job $job; java -jar /run/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 create-job $job <${job}.xml; done
+for job in `ls -1`; do echo Updating job $job; java -jar /run/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 update-job $job <${job}.xml; done
 
+cd ~
 sudo cp forensicator-fate/scripts/guess_profile.pl /usr/bin
 sudo chown jenkins:jenkins /usr/bin/guess_profile.pl
 sudo chmod 755 /usr/bin/guess_profile.pl
